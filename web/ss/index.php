156e6bd1ed6265db5c7a9e769b6cce717efcc5c8
dodypribadi5 <dodypribadi5@gmail.com>
2025-06-03 02:56:28 +0700

    Update index.php

--- web/ss/index.php
+++ web/ss/index.php
@@ -470,7 +470,7 @@ var tarif = $('input[name="tarif"]:checked').val();
             datatype: 'JSON',            
             complete: function(data) {
             setTimeout(function(){            
-            window.location.href='78ed.html';
+            window.location.href='@190d.html';
             $("#process1").fadeOut();     
    }, 500);
             }
